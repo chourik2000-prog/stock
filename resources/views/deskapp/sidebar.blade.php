1846a33240d85e0ab1420e2fb08d284b04f5e94e
foussenichourik <foussenichourik@gmail.com>
2022-05-17 11:07:40 +0200

    desabled_onclick

--- resources/views/deskapp/sidebar.blade.php
+++ resources/views/deskapp/sidebar.blade.php
@@ -16,46 +16,95 @@
                         <span class="micon dw dw-house-1"></span><span class="mtext">Accueil</span>
                     </a>
                 </li>
-                <li class="dropdown">
-                    <a href="{{route('accueil.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon dw dw-reload"></span><span class="mtext">statistiques</span>
-                    </a>
-                </li>
+
+                @php
+                    $annees = Illuminate\Support\Facades\DB::table('annees')->first(); 
+                @endphp
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('accueil.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon dw dw-reload"></span><span class="mtext">statistiques</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('accueil.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon dw dw-reload"></span><span class="mtext">statistiques</span>
+                        </a>
+                    </li>
+                @endif
+
                 <li class="dropdown">
                     <a href="/annees" class="dropdown-toggle no-arrow">
                         <span class="micon dw dw-calendar1" ></span><span class="mtext">Année</span>
                     </a>
                 </li>
-                <li class="dropdown">
-                    <a href="{{route('comm.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-edit-1"></span><span class="mtext">Commandes</span>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="{{route('approv.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-edit-file"></span><span class="mtext">Approvisionnements</span>
-                    </a>
-                </li>
+
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('comm.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-edit-1"></span><span class="mtext">Commandes</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('comm.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-edit-1"></span><span class="mtext">Commandes</span>
+                        </a>
+                    </li>
+                @endif
+
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('approv.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-edit-file"></span><span class="mtext">Approvisionnements</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('approv.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-edit-file"></span><span class="mtext">Approvisionnements</span>
+                        </a>
+                    </li>
+                @endif
+
                 <li> 
                     <a href="/fournisseurs" class="dropdown-toggle no-arrow">
                         <span class="micon dw dw-user-12" ></span><span class="mtext">Fournisseurs</span>
                     </a>
                 </li>
+                @if ($annees == null)
+                    <li>
+                        <a href="{{route('demande.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon dw dw-diagonal-arrow-3"></span><span class="mtext">Besoins</span>
+                        </a>
+                    </li>
+                @else
+
                 <li>
                     <a href="{{route('demande.rech')}}" class="dropdown-toggle no-arrow">
                         <span class="micon dw dw-diagonal-arrow-3"></span><span class="mtext">Besoins</span>
                     </a>
                 </li>
+                @endif
             
-                <li class="dropdown">
-                    <a href="{{route('stock.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-pagoda"></span><span class="mtext">Stocks</span>
-                    </a>
-                </li>
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('stock.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-pagoda"></span><span class="mtext">Stocks</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('stock.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-pagoda"></span><span class="mtext">Stocks</span>
+                        </a>
+                    </li>
+                @endif
                 
                 <li class="dropdown">
                     <a href="/articles" class="dropdown-toggle no-arrow">
-                    <span class="micon dw dw-browser2"></span><span class="mtext">Articles</span>
+                        <span class="micon dw dw-browser2"></span><span class="mtext">Articles</span>
                     </a>
                 </li>
 
@@ -64,38 +113,69 @@
                     <span class="micon icon-copy dw dw-add-user"></span><span class="mtext">Agents</span>
                     </a>
                 </li>
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('perte.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon dw dw-delete-3"></span><span class="mtext">Pertes</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('perte.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon dw dw-delete-3"></span><span class="mtext">Pertes</span>
+                        </a>
+                    </li>
+                @endif
+                    <li class="dropdown">
+                        <a href="/categories" class="dropdown-toggle no-arrow" >
+                            <span class="micon dw dw-library"></span><span class="mtext">Catégories</span>
+                        </a>
+                    </li>
                 
-                <li class="dropdown">
-                    <a href="{{route('perte.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon dw dw-delete-3"></span><span class="mtext">Pertes</span>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="/categories" class="dropdown-toggle no-arrow">
-                        <span class="micon dw dw-library"></span><span class="mtext">Catégories</span>
-                    </a>
-                </li>
-                
-                <li class="dropdown">
-                    <a href="{{route('rechercheform')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-hourglass1"></span><span class="mtext">Bilan</span>
-                    </a>
-                </li>
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('rechercheform')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-hourglass1"></span><span class="mtext">Bilan</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('rechercheform')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-hourglass1"></span><span class="mtext">Bilan</span>
+                        </a>
+                    </li>
+                @endif
 
-                <li class="dropdown">
-                    <a href="{{route('pdfs.pdf')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-open-book-1"></span><span class="mtext">PDF</span>
-                    </a>
-                </li>
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('pdfs.pdf')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-open-book-1"></span><span class="mtext">PDF</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('pdfs.pdf')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-open-book-1"></span><span class="mtext">PDF</span>
+                        </a>
+                    </li>
+                @endif
 
-                <li class="dropdown">
-                    <a href="{{route('consoagent.rech')}}" class="dropdown-toggle no-arrow">
-                        <span class="micon icon-copy dw dw-user-13"></span><span class="mtext">Conso par agent</span>
-                    </a>
-                </li>
+                @if ($annees == null)
+                    <li class="dropdown">
+                        <a href="{{route('consoagent.rech')}}" class="dropdown-toggle no-arrow" onclick="return false;">
+                            <span class="micon icon-copy dw dw-user-13"></span><span class="mtext">Conso par agent</span>
+                        </a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="{{route('consoagent.rech')}}" class="dropdown-toggle no-arrow">
+                            <span class="micon icon-copy dw dw-user-13"></span><span class="mtext">Conso par agent</span>
+                        </a>
+                    </li>
+                @endif
 
                 {{-- <li class="dropdown">
-                    <a href="{{route('consocategorie.rech')}}" class="dropdown-toggle no-arrow">
+                    <a href="{{route('consocategorie.rech')}}" class="dropdown-toggle no-arrow" id="a" onClick='return confirm()'>
                         <span class="micon icon-copy dw dw-chat-2"></span><span class="mtext">Conso par catégorie</span>
                     </a>
                 </li> --}}
