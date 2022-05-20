0fd45a7242fd1c6d716a01057b0b1551127d9294
foussenichourik <foussenichourik@gmail.com>
2022-05-20 11:12:32 +0200

    cat_agent

--- resources/views/categories/modalmodifier.blade.php
+++ resources/views/categories/modalmodifier.blade.php
@@ -1,26 +1,25 @@
 <div class="modal fade" id="modaledit{{$categorie->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-          <div class="modal-content">
-              <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Catégorie de personnel</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                      </button>
-              </div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modifier </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
             <div class="modal-body">
-                <form action="{{ route('categories.update',$categorie->id) }}" method="POST">
+                <form action="{{ route('categos.update',$categorie->id) }}" method="POST">
                     @csrf
                     @method('PUT')
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <label for="libelle">Catégorie:</label>
+                                   <label for="name">Libellé:</label>
                                     <input type="text" 
-                                    name="libelle" id="libelle"
+                                    name="libelle" id="name"
                                     value="{{ $categorie->libelle }}" 
-                                    class="form-control @error('libelle') is-invalid @enderror" 
-                                    placeholder="désignation" required="required">
-                                    @error('libelle') <p>Ce champs est incorrect</p>@enderror
+                                    class="form-control @error('libelle') is-invalid @enderror"
+                                    placeholder="libellé" required="required">
                                 </div>
                             </div>
                         </div>
@@ -30,13 +29,12 @@
                             </div>
                         </div>
                 </form>
-            </div>  
+            </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                <button type="submit" class="btn btn-primary">Enregistrer</button>
-            </div> 
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+            </div>       
         </div>
     </div>
 </div>
-
   
+    
