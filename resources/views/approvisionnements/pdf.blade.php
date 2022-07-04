f74d7116ad52c117a6dc36c6ea17f51845d4ef8a
foussenichourik <foussenichourik@gmail.com>
2022-07-04 14:55:38 +0200

    pdf statistique

--- resources/views/approvisionnements/pdf.blade.php
+++ resources/views/approvisionnements/pdf.blade.php
@@ -8,24 +8,14 @@
             width: 700px;
             border-collapse:collapse;
         }
-        .thead {
+        thead th{
             border: 3px solid black;
             text-align: left;
         }
-        .th{
-            border: 3px solid black;
-            text-align: left;
-        }
-        .td {
+        td th{
             padding: 8px 10px;
         }
-        .th{
-            padding: 8px 10px;
-        }
-        .tbody {
-            border: 3px solid black;
-        }
-        .td {
+        tbody  td {
             border: 3px solid black;
         }
         #titre{
@@ -42,21 +32,21 @@
         <h2 id="titre"><strong>IAI-TOGO</strong></h2>
         <h2 class="entete">Liste des approvisionnements de l'année en cours</h2>
         <table class="tableau">
-            <thead class="thead">
-                <tr class="tr">
-                    <th class="th">Article</th>
-                    <th class="th">Fournisseur</th>
-                    <th class="th">Quantité</th>
-                    <th class="th">Date</th>
+            <thead>
+                <tr>
+                    <th>Article</th>
+                    <th>Fournisseur</th>
+                    <th>Quantité</th>
+                    <th>Date</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($approvisionnements as $approvisionnement)
-                    <tr class="tr">
-                        <td class="td">{{ $approvisionnement->article->libelle}}</td>
-                        <td class="td">{{ $approvisionnement->fournisseur->nom}}</td>
-                        <td class="td">{{ $approvisionnement->qentrant}}</td>
-                        <td class="td">{{ $approvisionnement->date}}</td>
+                    <tr>
+                        <td>{{ $approvisionnement->article->libelle}}</td>
+                        <td>{{ $approvisionnement->fournisseur->nom}}</td>
+                        <td>{{ $approvisionnement->qentrant}}</td>
+                        <td>{{ $approvisionnement->date}}</td>
                     </tr> 
                     @endforeach
             </tbody>
