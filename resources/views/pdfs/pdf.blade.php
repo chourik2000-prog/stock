43645ddd7bc3a883b3dcd607c6338aa1f6e48ec9
foussenichourik <foussenichourik@gmail.com>
2022-07-07 08:41:50 +0200

    fixed position

--- resources/views/pdfs/pdf.blade.php
+++ resources/views/pdfs/pdf.blade.php
@@ -8,7 +8,7 @@
             min-width: 400px;
             width: 700px;
             border-collapse:collapse;
-            position: relative;
+            position:fixed;
         }
         thead th{
             border: 2px solid black;
